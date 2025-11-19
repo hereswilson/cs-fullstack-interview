23b64a66877c1ffefa58895e96b63123101d50c0
Wilson Heres <wheres67@gmail.com>
2025-11-18 20:00:09 -0500

    add CORS support to the Flask app for localhost development

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
+from flask_cors import CORS
 
 ma = Marshmallow()
 db = SQLAlchemy()
@@ -11,6 +12,9 @@ migrate = Migrate()
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
+
+    CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
+
     ma.init_app(app)
     db.init_app(app)
     migrate.init_app(app, db)
